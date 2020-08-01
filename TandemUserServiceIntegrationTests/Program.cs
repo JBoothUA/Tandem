2b917be004b2f52d64edf8c1cc14ab6fa846a80a
Jesse <jesseboothua@gmail.com>
2020-08-01 01:57:09 -0500

    finished a health check, scaffolded an integration test console project

---
+++ TandemUserServiceIntegrationTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TandemUserServiceIntegrationTests
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tandem user Service Integration Tests!");
+        }
+    }
+}
