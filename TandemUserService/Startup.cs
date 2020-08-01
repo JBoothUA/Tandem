06d7b14ba351e47b1d4151e4433b315ff64606bb
Jesse <jesseboothua@gmail.com>
2020-08-01 01:27:52 -0500

    automap user to userdto

--- TandemUserService/Startup.cs
+++ TandemUserService/Startup.cs
@@ -4,6 +4,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
 using TandemUserService.Configuration;
 
 namespace TandemUserService
@@ -34,6 +37,9 @@ namespace TandemUserService
             services.AddSwaggerGen(swagger =>
             {
                 swagger.SwaggerDoc("v1", new OpenApiInfo { Title = "Tandem User Service API" });
+                var xmlFile = $"{ Assembly.GetExecutingAssembly().GetName().Name }.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                swagger.IncludeXmlComments(xmlPath);
             });
 
             // Add Mvc Routes
