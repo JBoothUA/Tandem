d9715aa84e5e5b71f1758a504c38cf1286020e18
Jesse <jesseboothua@gmail.com>
2020-08-08 21:26:03 -0500

    remove old files

--- DocumentService/Startup.cs
+++ DocumentService/Startup.cs
@@ -1,50 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DoucmentService.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using System;
-using System.IO;
-using System.Reflection;
-using TandemUserService.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace DocumentService
 {
     public class Startup
     {
-        public IConfiguration Configuration { get; }
-
-        public Startup(IHostEnvironment env)
+        public Startup(IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-
-            builder.AddEnvironmentVariables();
-            Configuration = builder.Build();
+            Configuration = configuration;
         }
+
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
-
-            // Allow sites to hit the API
-            services.AddCors();
-
-            // Add Swagger
-            services.AddSwaggerGen(swagger =>
-            {
-                swagger.SwaggerDoc("v1", new OpenApiInfo { Title = "Document Service API" });
-                var xmlFile = $"{ Assembly.GetExecutingAssembly().GetName().Name }.xml";
-            });
-
-            // Add Mvc Routes
-            services.AddMvc(option => option.EnableEndpointRouting = false);
+            services.AddControllersWithViews();
 
             // Add custom configuration
-            services.Configure<UserServiceConfig>(Configuration);
+            services.Configure<DocumentServiceConfig>(Configuration);
+
+            // Explicitly register the settings object by delegating to the IOptions object
+            services.AddSingleton(resolver =>
+                resolver.GetRequiredService<IOptions<DocumentServiceConfig>>().Value);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,19 +42,24 @@ namespace DocumentService
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            // Add Swagger
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
+            else
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Document API V1");
-                c.RoutePrefix = "api";
-            });
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
